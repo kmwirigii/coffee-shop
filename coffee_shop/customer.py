95867a2b9c819fa5c9903f7bf2bff4c23878708e
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-05-21 22:47:24 +0300

    implement name property

--- coffee_shop/customer.py
+++ coffee_shop/customer.py
@@ -9,3 +9,16 @@ class Customer:
         self._orders: List[Order] = []
         self.name = name
         Customer.all.append(self)
+
+         @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Name must be between 1-15 characters")
+        self._name = value
+
