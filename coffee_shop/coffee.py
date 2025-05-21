8f80a5990030783c0516af1b8e8f678d2871a6d1
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-05-21 22:31:06 +0300

    add order tracking

--- coffee_shop/coffee.py
+++ coffee_shop/coffee.py
@@ -10,3 +10,21 @@ class Coffee:
         self._name = name
         self._orders: List[Order] = []
         Coffee.all.append(self)
+
+        @property
+    def name(self) -> str:
+        return self._name
+    
+    
+    @name.setter
+    def name(self, value: str) -> None:
+       
+        if hasattr(self, '_name'):
+            raise AttributeError("Cannot change coffee name after creation")
+        self._validate_name(value)
+        self._name = value
+
+
+
+
+
