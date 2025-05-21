7bd4fff77b5f29bc13ae7f504785f07a0d46f2bc
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-05-21 23:10:25 +0300

    create price property

--- coffee_shop/order.py
+++ coffee_shop/order.py
@@ -14,3 +14,24 @@ if TYPE_CHECKING:
         self.customer = customer
         self.coffee = coffee
         self.price = price
+
+        
+        customer._orders.append(self)
+        coffee._orders.append(self)
+        Order.all.append(self)
+
+           @property
+    def price(self) -> float:
+        return self._price
+
+    @price.setter
+    def price(self, value: float) -> None:
+        if not isinstance(value, float):
+            raise ValueError("Price must be a float")
+        if not (1.0 <= value <= 10.0):
+            raise ValueError("Price must be between 1.0 and 10.0")
+        if hasattr(self, '_price'):
+            raise AttributeError("Cannot change order price after creation")
+        self._price = value
+
+
