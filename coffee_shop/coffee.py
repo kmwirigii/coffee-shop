17ded3a58ad015abf37eb9b80e847dea371dd005
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-05-21 22:27:05 +0300

    create coffee class

--- coffee_shop/coffee.py
+++ coffee_shop/coffee.py
@@ -1,2 +1,12 @@
 from __future__ import annotations
 from typing import List
+
+class Coffee:
+    
+    all: List[Coffee] = []
+
+    def __init__(self, name: str) -> None:
+        self._validate_name(name)
+        self._name = name
+        self._orders: List[Order] = []
+        Coffee.all.append(self)
