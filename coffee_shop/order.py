bb756758bcaf05d704dde071e337426b6d9a7093
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-05-21 23:07:48 +0300

    track orders

--- coffee_shop/order.py
+++ coffee_shop/order.py
@@ -4,3 +4,13 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .customer import Customer
     from .coffee import Coffee
+
+    class Order:
+    # Class-level list to store all orders
+    all: list[Order] = []
+
+    def __init__(self, customer: Customer, coffee: Coffee, price: float) -> None:
+        
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
